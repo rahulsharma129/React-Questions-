695622ecb5288e4e934aa9ce23873ae09fa63605
Rahul Sharma <rahulakash123456@gmail.com>
2025-09-26 23:24:55 +0530

    gg

--- Easy/Chips Problem.jsx
+++ Easy/Chips Problem.jsx
@@ -1,17 +1,53 @@
-import React, { useState } from "react";
-import './styles.css'
-function ChipsInput() {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return NULL;
+        if (root->val == key) return helper(root);
 
-  return (
-    <div className='main-container'>
-      <h2>Chips Input</h2>
-      <input
-        type="text" 
-        placeholder="Type a chip and press tag"
-        className="input"
-      />
-    </div>
-  );
-}
+        TreeNode* dummy = root;
+        while (root) {
+            if (root->val > key) {
+                if (root->left && root->left->val == key) {
+                    root->left = helper(root->left);
+                    break;
+                } else {
+                    root = root->left;
+                }
+            } else {
+                if (root->right && root->right->val == key) {
+                    root->right = helper(root->right);
+                    break;
+                } else {
+                    root = root->right;
+                }
+            }
+        }
+        return dummy;
+    }
 
-export default ChipsInput;
+    TreeNode* helper(TreeNode* root) {
+        if (!root->left) return root->right;
+        if (!root->right) return root->left;
+
+        TreeNode* rightChild = root->right;
+        TreeNode* lastRight = findLastRight(root->left);
+        lastRight->right = rightChild;
+        return root->left;
+    }
+
+    TreeNode* findLastRight(TreeNode* root) {
+        if (!root->right) return root;
+        return findLastRight(root->right);
+    }
+};
